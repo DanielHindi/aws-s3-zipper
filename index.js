89852277801a99bebcb5db39f8674af97ed7459a
Brad Fowler <bradley.fowler123@gmail.com>
2019-08-06 10:57:55 +0100

    Resolves #35

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ var s3 = require('s3');
 
 function S3Zipper(awsConfig) {
     var self = this
-    AWS.config.getCredentials(function (err) {
-      if (err) {
-        assert.ok(awsConfig, 'AWS S3 options must be defined.');
-        assert.notEqual(awsConfig.accessKeyId, undefined, 'Requires S3 AWS Key.');
-        assert.notEqual(awsConfig.secretAccessKey, undefined, 'Requires S3 AWS Secret');
-        assert.notEqual(awsConfig.region, undefined, 'Requires AWS S3 region.');
-        assert.notEqual(awsConfig.bucket, undefined, 'Requires AWS S3 bucket.');
-        self.init(awsConfig);
-      } else {
-        self.init(awsConfig)
-      }
-    })
+    assert.ok(awsConfig, 'AWS S3 options must be defined.');
+    assert.notEqual(awsConfig.accessKeyId, undefined, 'Requires S3 AWS Key.');
+    assert.notEqual(awsConfig.secretAccessKey, undefined, 'Requires S3 AWS Secret');
+    assert.notEqual(awsConfig.region, undefined, 'Requires AWS S3 region.');
+    assert.notEqual(awsConfig.bucket, undefined, 'Requires AWS S3 bucket.');
+
+    AWS.config.update({
+        accessKeyId: awsConfig.accessKeyId,
+        secretAccessKey: awsConfig.secretAccessKey,
+        region: awsConfig.region
+    });
+
+    self.init(awsConfig);
 }
 
 
@@ -31,30 +32,11 @@ S3Zipper.prototype = {
     init: function (awsConfig) {
         this.awsConfig = awsConfig;
         var self = this
-        AWS.config.getCredentials(function (err) {
-
-            if (err) {
-                AWS.config.update({
-                    accessKeyId: awsConfig.accessKeyId,
-                    secretAccessKey: awsConfig.secretAccessKey,
-                    region: awsConfig.region
-                });
+        self.s3bucket = new AWS.S3({
+            params: {
+                Bucket: self.awsConfig.bucket
             }
-
-            if (awsConfig.endpoint) {
-                AWS.config.update({
-                    endpoint: awsConfig.endpoint
-                });
-            }
-
-            self.s3bucket = new AWS.S3({
-                params: {
-                    Bucket: self.awsConfig.bucket
-                }
-            });
-
-        })
-
+        });
     }
     , filterOutFiles: function (fileObj) {
         return fileObj;
